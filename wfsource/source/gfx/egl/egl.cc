9c813f88c13f628a57b3fb7093f2313211659937
Kevin Seghetti <kseghetti@artlogic.net>
2012-12-31 12:11:24 -0800

    some cleanup

--- wfsource/source/gfx/egl/egl.cc
+++ wfsource/source/gfx/egl/egl.cc
@@ -25,10 +25,7 @@
 // Original Author: Kevin T. Seghetti
 //============================================================================
 
-#include <time.h>
-
-//==============================================================================
-
+// kts why is this here? 
 struct HalDisplay
 {
     XDisplay *mainDisplay;
@@ -44,21 +41,8 @@ struct HalDisplay
 HalDisplay halDisplay;
 
 //===============================================================================
-/**************************************************************************
- *
- * Copyright 2008 Tungsten Graphics, Inc., Cedar Park, Texas.
- * All Rights Reserved.
- *
- **************************************************************************/
 
-/*
- * Draw a triangle with X/EGL and OpenGL ES 2.x
- */
-
-#define XLIB_ILLEGAL_ACCESS 1
-
-#define USE_FULL_GL 0
-
+#include <time.h>
 #include <assert.h>
 #include <math.h>
 #include <stdlib.h>
@@ -67,75 +51,11 @@ HalDisplay halDisplay;
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/keysym.h>
-#if USE_FULL_GL
-#include <GL/gl.h>  /* use full OpenGL */
-#else
 #include <GLES2/gl2.h>  /* use OpenGL ES 2.x */
-#endif
 #include <EGL/egl.h>
 
-#define FLOAT_TO_FIXED(X)   ((X) * 65535.0)
-static GLfloat view_rotx = 0.0, view_roty = 0.0;
 static GLint u_matrix = -1;
 static GLint attr_pos = 0, attr_color = 1;
-
-//static void
-//make_z_rot_matrix(GLfloat angle, GLfloat *m)
-//{
-//   float c = cos(angle * M_PI / 180.0);
-//   float s = sin(angle * M_PI / 180.0);
-//   int i;
-//   for (i = 0; i < 16; i++)
-//      m[i] = 0.0;
-//   m[0] = m[5] = m[10] = m[15] = 1.0;
-//
-//   m[0] = c;
-//   m[1] = s;
-//   m[4] = -s;
-//   m[5] = c;
-//}
-//
-//static void
-//make_scale_matrix(GLfloat xs, GLfloat ys, GLfloat zs, GLfloat *m)
-//{
-//   int i;
-//   for (i = 0; i < 16; i++)
-//      m[i] = 0.0;
-//   m[0] = xs;
-//   m[5] = ys;
-//   m[10] = zs;
-//   m[15] = 1.0;
-//}
-//
-//
-//static void
-//mul_matrix(GLfloat *prod, const GLfloat *a, const GLfloat *b)
-//{
-//#define A(row,col)  a[(col<<2)+row]
-//#define B(row,col)  b[(col<<2)+row]
-//#define P(row,col)  p[(col<<2)+row]
-//   GLfloat p[16];
-//   GLint i;
-//   for (i = 0; i < 4; i++) {
-//      const GLfloat ai0=A(i,0),  ai1=A(i,1),  ai2=A(i,2),  ai3=A(i,3);
-//      P(i,0) = ai0 * B(0,0) + ai1 * B(1,0) + ai2 * B(2,0) + ai3 * B(3,0);
-//      P(i,1) = ai0 * B(0,1) + ai1 * B(1,1) + ai2 * B(2,1) + ai3 * B(3,1);
-//      P(i,2) = ai0 * B(0,2) + ai1 * B(1,2) + ai2 * B(2,2) + ai3 * B(3,2);
-//      P(i,3) = ai0 * B(0,3) + ai1 * B(1,3) + ai2 * B(2,3) + ai3 * B(3,3);
-//   }
-//   memcpy(prod, p, sizeof(p));
-//#undef A
-//#undef B
-//#undef PROD
-//}
-
-/* new window size or exposure */
-static void
-reshape(int width, int height)
-{
-   glViewport(0, 0, (GLint) width, (GLint) height);
-}
-
 
 static void
 create_shaders(void)
@@ -234,22 +154,6 @@ create_shaders(void)
 }
 
 
-static void
-init(void)
-{
-   typedef void (*proc)();
-
-#if 1 /* test code */
-   proc p = eglGetProcAddress("glMapBufferOES");
-   assert(p);
-#endif
-
-   glClearColor(0.4, 0.4, 0.4, 0.0);
-
-   create_shaders();
-}
-
-
 /*
  * Create an RGB, double-buffered X window.
  * Return the window and context handles.
@@ -334,11 +238,7 @@ make_x_window(XDisplay *x_dpy, EGLDisplay egl_dpy,
                               None, (char **)NULL, 0, &sizehints);
    }
 
-#if USE_FULL_GL /* XXX fix this when eglBindAPI() works */
-   eglBindAPI(EGL_OPENGL_API);
-#else
    eglBindAPI(EGL_OPENGL_ES_API);
-#endif
 
    ctx = eglCreateContext(egl_dpy, config, EGL_NO_CONTEXT, ctx_attribs );
    if (!ctx) {
@@ -376,21 +276,10 @@ make_x_window(XDisplay *x_dpy, EGLDisplay egl_dpy,
    *ctxRet = ctx;
 }
 
-
-static void
-usage(void)
-{
-   printf("Usage:\n");
-   printf("  -display <displayname>  set the display to run on\n");
-   printf("  -info                   display OpenGL renderer info\n");
-}
-
-
 void 
 OpenMainWindow( char *title, int winWidth, int winHeight)
 {
    char *dpyName = NULL;
-   GLboolean printInfo = GL_FALSE;
    EGLint egl_major, egl_minor;
    int i;
    const char *s;
@@ -436,24 +325,24 @@ OpenMainWindow( char *title, int winWidth, int winHeight)
       exit -1;
    }
 
-   if (printInfo) {
-      printf("GL_RENDERER   = %s\n", (char *) glGetString(GL_RENDERER));
-      printf("GL_VERSION    = %s\n", (char *) glGetString(GL_VERSION));
-      printf("GL_VENDOR     = %s\n", (char *) glGetString(GL_VENDOR));
-      printf("GL_EXTENSIONS = %s\n", (char *) glGetString(GL_EXTENSIONS));
-   }
-
-   init();
+   glClearColor(0.4, 0.4, 0.4, 0.0);
+   create_shaders();
 
 }  
 
+
+// new window size
+static void
+reshape(int width, int height)
+{
+   glViewport(0, 0, (GLint) width, (GLint) height);
+}
 
 
 bool
 InitWindow( int /*xPos*/, int /*yPos*/, int /*xSize*/, int /*ySize*/ )
 {
    const int winWidth = 500, winHeight = 300;
-
     OpenMainWindow( "World Foundry", winWidth, winHeight);
 
     /* Set initial projection/viewing transformation.
@@ -461,10 +350,8 @@ InitWindow( int /*xPos*/, int /*yPos*/, int /*xSize*/, int /*ySize*/ )
      * first appears.
      */
     reshape(winWidth, winHeight);
-
     return true;
 }
-
 
 void
 DestroyWindow(void)
@@ -473,16 +360,11 @@ DestroyWindow(void)
    eglDestroySurface(halDisplay.eglDisplay, halDisplay.eglSurface);
    eglTerminate(halDisplay.eglDisplay);
 
-
    XDestroyWindow(halDisplay.mainDisplay, halDisplay.win);
    XCloseDisplay(halDisplay.mainDisplay);
 
    exit (0);
 }
-
-//===============================================================================
-
-//===============================================================================
 
 //===============================================================================
 
