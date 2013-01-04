7a4fa32fccacd61aa4bf229dddcd809b49c987f8
Kevin Seghetti <kseghetti@artlogicdev.net>
2013-01-03 17:23:11 -0800

    made error print in hex, since that is how they are defined. added commented out reference chart of error values

--- wfsource/source/gfx/egl/wfprim.h
+++ wfsource/source/gfx/egl/wfprim.h
@@ -17,6 +17,16 @@
 
 //==============================================================================
         
+        
+//#define GL_NO_ERROR 				0x0
+//#define GL_INVALID_ENUM				0x0500
+//#define GL_INVALID_VALUE			0x0501
+//#define GL_INVALID_OPERATION			0x0502
+//#define GL_STACK_OVERFLOW			0x0503
+//#define GL_STACK_UNDERFLOW			0x0504
+//#define GL_OUT_OF_MEMORY			0x0505
+        
+        
 #define AssertGLOK()        AssertGLOKMsg("")
                                      
 #if 1
@@ -32,7 +42,7 @@
     { \
         while(glError != GL_NO_ERROR) \
         { \
-            DBSTREAM1(cerror << "GL error: " << glError << std::endl; \
+            DBSTREAM1(cerror << "GL error: 0x" << std::hex << glError << std::endl; \
             cerror << "WFError: " << string << std::endl;) \
             glError = glGetError(); \
         } \
